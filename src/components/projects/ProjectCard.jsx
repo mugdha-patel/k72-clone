01ea39c6f871408b2805fadce178c6008c27cae4
Mugdha <mugdha220598@gmail.com>
2025-10-19 23:59:44 +0100

    projects page

---
+++ src/components/projects/ProjectCard.jsx
@@ -0,0 +1,25 @@
+
+import React from 'react'
+
+const ProjectCard = (props) => {
+   
+
+  return (
+    <>
+          <div className="h-full group relative rounded-none w-1/2 hover:rounded-[40px] overflow-hidden">
+            <img className='h-full w-full object-cover' src={props.image1} alt="" />
+            <div className='absolute transition-all  opacity-0 group-hover:opacity-100 flex items-center justify-center pt-2 top-0 left-0 h-full w-full bg-black/10'>
+              <h2 className='uppercase font-[fontprimary] px-8 pt-2  text-6xl border-2 text-white border-white rounded-full '>See the project</h2>
+            </div>
+          </div>
+          <div className="h-full group relative rounded-none w-1/2 hover:rounded-[40px] overflow-hidden">
+            <img className='h-full w-full object-cover' src={props.image2} alt="" />
+            <div className='absolute transition-all  opacity-0 group-hover:opacity-100 flex items-center justify-center pt-2 top-0 left-0 h-full w-full bg-black/10'>
+              <h2 className='uppercase font-[fontprimary] px-8 pt-2  text-6xl border-2 text-white border-white rounded-full '>See the project</h2>
+            </div>
+          </div>
+    </>
+  )
+}
+
+export default ProjectCard
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,8 +1,49 @@
 import React from 'react'
+import ProjectCard from '../components/projects/ProjectCard'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
 
 const Projects = () => {
+  const projects = [{
+    image1: 'https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960-1280x960.jpg',
+    image2: 'https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---Thumbnail-1280x960.jpg'
+  }, {
+    image1: 'https://k72.ca/uploads/caseStudies/OKA/OKA_thumbnail-1280x960.jpg',
+    image2: 'https://k72.ca/uploads/caseStudies/Opto/thumbnailimage_opto-1280x960.jpg'
+  }, {
+    image1: 'https://k72.ca/uploads/caseStudies/LAMAJEURE_-_Son_sur_mesure/chalaxeur-thumbnail_img-1280x960.jpg',
+    image2: 'https://k72.ca/uploads/caseStudies/SHELTON/thumbnailimage_shelton-1280x960.jpg'
+  }]
+
+  gsap.registerPlugin(ScrollTrigger)
+  useGSAP(function () {
+    gsap.from('.hero', {
+      height: '20px',
+      stagger: {
+        amount: 0.4
+      },
+      scrollTrigger: {
+        trigger: '.cardmain',
+        start: 'top 100%',
+        end: 'top -150%',
+        scrub: true
+      }
+    })
+  })
   return (
-    <div>Projects</div>
+    <div className='p-4'>
+      <div className='pt-[30vh] '>
+        <h2 className='font-[fontsecondary] text-[12vw] uppercase'>Projects</h2>
+      </div>
+      <div className='-mt-10 cardmain'>
+        {projects.map((elem, idx) => {
+          return <div key={idx} className="hero w-full  h-[500px] mb-4 flex gap-4 ">
+            <ProjectCard image1={elem.image1} image2={elem.image2} />
+          </div>
+        })}
+      </div>
+    </div>
   )
 }
 
