9d9ee6610c998a957192f7669afe7095283e9937
Mugdha <mugdha220598@gmail.com>
2025-10-21 00:09:40 +0100

    conflits resolved

--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import bannervideo from '../../assets/bannervideo.mp4'
 
 const Video = () => {
   return (
     <div className='h-full w-full'>
-        <video autoPlay muted loop className='h-full w-full object-cover' src='../../../public/bannervideo.mp4' ></video>
+        <video autoPlay muted loop className='h-full w-full object-cover' src={bannervideo} ></video>
     </div>
   )
 }
